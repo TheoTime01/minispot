a4c5f7096ed641e63653ccd708163cc9a74f4178
TheoTime01 <perricht@gmail.com>
2024-12-12 17:02:39 +0100

    ok

--- quadruped_robot/setup.py
+++ quadruped_robot/setup.py
@@ -8,13 +8,16 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[('share/ament_index/resource_index/packages',
-                 ['resource/' + package_name]),
-                (os.path.join('share', package_name), ['package.xml']),
-                (os.path.join('share', package_name,
-                              'launch'), glob('launch/*launch.[pxy][yma]*')),
-                (os.path.join('share', package_name,
-                              'config'), glob('config/*'))],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch' ), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config' ), glob('config/*')),
+        (os.path.join('share', package_name, 'urdf'   ), glob('urdf/*')),
+        (os.path.join('share', package_name, 'stl'   ), glob('stl/*')),
+    ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='root',
@@ -24,6 +27,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts':
-        ['servo_controller_node = quadruped_robot.servo_controller_node:main'],
+        [
+        'servo_controller_node = quadruped_robot.servo_controller_node:main',
+        'leg_controller = quadruped_robot.leg_controller:main'
+        ]
     },
 )
